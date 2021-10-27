73bc764e9b041b50eba8ee905eb4c151bbe74c7c
Opilane TTHK <opilane@tthk.ee>
2021-10-27 09:43:25 +0300

    week9

--- FileSystem/Program.cs
+++ FileSystem/Program.cs
@@ -8,10 +8,24 @@ namespace FileSystem
         static void Main(string[] args)
         {
             string rootDirectory = @"C:\Users\opilane\samples";
-            string userDirectory = "wishList";
+            Console.WriteLine("Enter directory name:");
+            string userDirectory = Console.ReadLine();
 
             string newDirectoryfullPath = @$"{rootDirectory}\{userDirectory}";
             Console.WriteLine(newDirectoryfullPath);
+
+            bool directoryExists = Directory.Exists(newDirectoryfullPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{userDirectory} already exists in {rootDirectory}");
+
+            }
+            else
+            {
+                Directory.CreateDirectory(newDirectoryfullPath);
+                Console.WriteLine($"Directory {userDirectory} has been created");
+            }
 
             Directory.CreateDirectory(newDirectoryfullPath);
 
